b25e6a01b8a01e8e0cf9893179985d1ea30a9d6d
Ivan Cavalheiro <ivanscavalheiro@gmail.com>
2017-07-24 11:47:45 -0300

    Added the basic DLL project to the solution where the plugin will be developed

---
+++ UCM/UmbracoContentMerger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCM
+{
+    class UmbracoContentMerger
+    {
+        public string GetVersion()
+        {
+            return "0.0.1";
+        }
+    }
+}
