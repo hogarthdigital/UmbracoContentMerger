01d23317c24a8523e0250fa0c8b234f400fb8a33
Ivan Cavalheiro <ivanscavalheiro@gmail.com>
2017-08-14 19:14:39 -0300

    Added the basic controller for the backoffice requests

---
+++ src/UCM/Controllers/UCMController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Editors;
+
+namespace UCM.Controllers
+{
+    public class UCMController : UmbracoAuthorizedJsonController
+    {
+        public object Index()
+        {
+            return new { mensagem = "qualquer merda" };
+        }
+    }
+}
