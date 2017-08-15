eed5f6cb7cd33c04046f8673c6034466838c5a1e
Ivan Cavalheiro <ivanscavalheiro@gmail.com>
2017-08-15 10:53:56 -0300

    Set up the basic ucm controller and ajax library

--- src/UCM/Controllers/UCMController.cs
+++ src/UCM/Controllers/UCMController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Umbraco.Web.Editors;
+using Umbraco.Web.WebApi;
 
 namespace UCM.Controllers
 {
-    public class UCMController : UmbracoAuthorizedJsonController
+    public class UCMController : UmbracoAuthorizedApiController
     {
+        [HttpPost]
         public object Index()
         {
             return new { mensagem = "qualquer merda" };
