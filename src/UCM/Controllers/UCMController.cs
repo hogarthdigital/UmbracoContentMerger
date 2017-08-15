f5519c2e4b20c7e331b8ebea49156b874c44d9d3
Ivan Cavalheiro <ivanscavalheiro@gmail.com>
2017-08-15 14:17:05 -0300

    Added model types and started the basic content listing action

--- src/UCM/Controllers/UCMController.cs
+++ src/UCM/Controllers/UCMController.cs
@@ -3,15 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
 using Umbraco.Web.WebApi;
 
 namespace UCM.Controllers
 {
     public class UCMController : UmbracoAuthorizedApiController
     {
+        [Serializable]
+        public class ContentType
+        {
+            public string Name;
+            public string[] Fields;
+
+            public ContentType(IContentType type)
+            {
+                Name = type.Name;
+                Fields = (from t in type.PropertyTypes
+                          select t.Name).ToArray();
+            }
+        }
+
+        [Serializable]
+        public class ContentModel
+        {
+            public string Name;
+            public int Id;
+            public Guid Guid;
+            public ContentType Type;
+            public ContentModel[] children;
+            public ContentModel(IContent content)
+            {
+                Name = content.Name;
+                Id = content.Id;
+                Guid = content.Key;
+                Type = new ContentType(content.ContentType);
+            }
+        }
+
+        private IContentService _cs { get { return ApplicationContext.Services.ContentService; } }
+
+        [HttpPost]
         public object Index()
         {
             return new { };
+        }
+
+
+        [System.Web.Http.HttpPost]
+        public object GetContentTree()
+        {
+            
+            return _cs.GetRootContent();
         }
     }
 }
