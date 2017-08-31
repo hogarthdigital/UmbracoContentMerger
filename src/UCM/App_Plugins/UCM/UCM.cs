e05e1a7206e9c450d7b9ff181de716f03e85faf1
Ivan S Cavalheiro <ivanscavalheiro@gmail.com>
2017-08-31 12:39:47 -0300

    Added new backoffice ui interface

---
+++ src/UCM/App_Plugins/UCM/UCM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using umbraco.interfaces;
+using umbraco.businesslogic;
+
+namespace UCM.App_Plugins.UCM
+{
+    [Application("ucm.umbraco.content.merger.tool", "UCM - Umbraco Content Merger", "icon-untitled", 12)]
+    public class UCM : IApplication
+    {
+    }
+}
